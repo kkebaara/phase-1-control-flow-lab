deb6d25aac8a3828ba3e193ffe5ce32319a6065d
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-14 00:56:52 -0700

    commit tree c9d061e68e215dc62fcd3c39742f2aae1d8d9d39

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  } else if (feet > 2500){
+    return 'No can do.'
+  } else {
+    return 'I will gladly take your thirty bucks.'
+  }
+   
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  };
+  
 }
